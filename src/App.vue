17686d8a89dbbb609ec1aab61e79309b6218bac6
SantiagoRP7 <santiago.19988@gmail.com>
2020-02-08 09:30:49 -0500

    designing

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,59 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="#949599"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+      <div>almas s</div>
+      </div>
+
+    </v-app-bar> 
+
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+
+    <v-navigation-drawer
+        v-model="drawer"
+        absolute
+        temporary
+      >
+      <v-list nav dense>
+        <v-list-item-group>
+          <v-list-item>
+            Pagos
+          </v-list-item>
+
+          <v-list-item>
+            Contador
+          </v-list-item>
+
+          <v-list-item>
+            span
+          </v-list-item>
+
+        </v-list-item-group>
+      </v-list>
+      </v-navigation-drawer>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld';
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  name: 'App',
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+    drawer: false,
+  }),
+};
+</script>
